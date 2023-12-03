b4c58efe46a75b2573e41ef350e7f0eacb9f5330
Vladimir Dulenov <originalrad50@gmail.com>
2023-12-03 22:41:31 +0100

    feat: PoC of listView is ready

---
+++ internal/prompt/completion.go
@@ -0,0 +1,32 @@
+package prompt
+
+import "github.com/charmbracelet/bubbles/list"
+
+var (
+	cityMap = map[string]struct{}{
+		"berlin":    {},
+		"munchen":   {},
+		"frankfurt": {},
+		"leipzig":   {},
+		"longon":    {},
+		"paris":     {},
+		"liverpool": {},
+		"koln":      {},
+		"lion":      {},
+		"flensburg": {},
+		"bordo":     {},
+		"erfurt":    {},
+		"dresden":   {},
+	}
+)
+
+func getCityNamesCompletion() []list.Item {
+	// we know that there are 13 items in the map
+	items := make([]list.Item, 13)
+
+	for cityName, _ := range cityMap {
+		items = append(items, item(cityName))
+	}
+	return items
+
+}
--- internal/prompt/view.go
+++ internal/prompt/view.go
@@ -3,7 +3,9 @@ package prompt
 import (
 	"github.com/charmbracelet/lipgloss"
 	//tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/textarea"
 )
 
 const listHeight = 10
@@ -11,10 +13,23 @@ const listWidth = 30
 const tableColumnWidth = 30
 const commandTableColumnName = "Command names"
 
-var baseStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.NormalBorder()).
-	BorderForeground(lipgloss.Color("240"))
+var (
+	baseStyle = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("240"))
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.Copy().PaddingLeft(4).PaddingBottom(1)
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("150"))
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+)
 
+// item implements list.Item interface
+type item string
+
+func (i item) FilterValue() string { return string(i) }
+
+// View returns the string representation of the main view
 func (m Model) View() string {
 
 	//table := baseStyle.Render(m.table.View())
@@ -54,4 +69,44 @@ func createTableTemplate(columns []table.Column, rows []table.Row) table.Model {
 		Bold(false)
 	t.SetStyles(s)
 	return t
+}
+
+// createTextArea creates a text area with the given text
+func createTextArea(text string) textarea.Model {
+	ta := textarea.New()
+	ta.Placeholder = "Flag description"
+	ta.BlurredStyle.Base = lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("238"))
+	ta.SetValue(text)
+	ta.ShowLineNumbers = false
+	ta.Blur()
+	return ta
+}
+
+func createListView(flagName string) list.Model {
+	// items contains results of available autocompletion result
+	var items []list.Item
+
+	// flags whose values can be received from fake-api
+	switch flagName {
+	case "name":
+		items = getCityNamesCompletion()
+	case "":
+		items = []list.Item{}
+	default:
+		items = []list.Item{item("No items found.")}
+	}
+
+	l := list.New(items, itemDelegate{}, listWidth, listHeight)
+	l.Title = "Available options"
+
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(true)
+	l.DisableQuitKeybindings()
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	return l
 }
